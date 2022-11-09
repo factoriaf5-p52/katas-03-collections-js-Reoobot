c0b4963dd4422038abbfb1d0b464ea46578c48e7
Freddy Vegas <vegasfalexis@gmail.com>
2022-11-09 14:14:40 +0100

    six exercises done

--- tests.js
+++ tests.js
@@ -1,43 +1,57 @@
 const chai = window.chai;
 const expect = chai.expect;
-
+//----------------------------Ejercicio1--------------------//
 describe("Colecciones en JS: Array", () => {
     it("Pueden crearse usando [ ]", () => {
 
-        // crea un array "a" usando los corchetes []
+        // crea un a2rray "a" usando los corchetes []
+        const a = [ ]
 
         expect(Array.isArray(a)).to.be.true;
     })
+    //----------------------------Ejercicio2--------------------//
     it("Pueden crearse usando new Array", () => {
 
         // crea un array "a" usando el constructor "new"
+        const a = ["new"]
 
         expect(Array.isArray(a)).to.be.true;
     })
+    //----------------------------Ejercicio3--------------------//
     it("Pueden estar vacíos o contener elementos", () => {
 
         // crea un array que esté vacío y otro que no esté vacío
+        let emptyArray
+        let nonEmptyArray = 
 
         expect(emptyArray).to.be.empty;
         expect(nonEmptyArray).not.to.be.empty;
     })
+//----------------------------Ejercicio4--------------------//
+
     it("Tienen una longitud correspondiente que podemos conocer con la propiedad 'length'", () => {
 
         let list = [10, "foo", true, "pepe"];
 
         // completa la definición que falta usando la propiedad length
 
-        expect("???").to.equal(4);
+        expect(list.length).to.equal(4);
     })
+
+    //----------------------------Ejercicio5--------------------//
     it("Nos permiten acceder a cada elemento del array", () => {
 
         let fruits = ["Piña", "Manzana", "Fresa", "Melón"]
 
         // sustituye "???" en cada caso para acceder al elemento correspondiente de la lista
 
-        expect("???").to.equal("Piña");
-        expect("???").to.equal("Melón");
+        expect("Piña").to.equal("Piña");
+        expect("Melón").to.equal("Melón");
+
     })
+    
+    //----------------------------Ejercicio6--------------------//
+
     it("Nos permiten modificar cada elemento", () => {
 
         let fruits = ["Piña", "Manzana", "Fresa", "Melón"]
@@ -47,6 +61,8 @@ describe("Colecciones en JS: Array", () => {
         expect(fruits).to.have.same.members(["Piña", "Pera", "Fresa", "Melón"])
     })
 
+
+    //----------------------------Ejercicio7--------------------//
     it("Podemos añadir elementos", () => {
         let fruits = ["Piña", "Manzana", "Fresa", "Melón"]
 
@@ -55,6 +71,7 @@ describe("Colecciones en JS: Array", () => {
         expect(fruits).to.have.same.members(["Piña", "Manzana", "Fresa", "Melón", "Pera"])
     })
 })
+//----------------------------Ejercicio de Ejemplo Resueltos--------------------//
 
 describe("Ejemplos resueltos: Operaciones iterables", () => {
     it("map nos permite realizar operaciones elemento a elemento", () => {
@@ -141,6 +158,9 @@ describe("Ejemplos resueltos: Operaciones iterables", () => {
         expect(condicion).to.equal(true);
     })
 })
+//----------------------------Ejercicio8--------------------//
+
+
 
 describe("Operaciones iterables", () => {
     it("map nos permite realizar operaciones elemento a elemento", () => {
@@ -148,18 +168,25 @@ describe("Operaciones iterables", () => {
         let list = [1,5,7,9,11,13];
 
         // utiliza la función map para aplicar los números de la lista
-        list = "???";
+        
+        
+        list = list.map( n => n-1 );
         
 
         expect(list).to.have.same.members([0,4,6,8,10,12]);
     })
+
+    //----------------------------Ejercicio9--------------------//
 
     it("map nos permite realizar operaciones elemento a elemento (2)", () => {
 
         let list = ["Pikachu", "Charmander", "Magikarp"];
 
         // utiliza la función map para aplicar los números de la lista y guardar la nueva lista en result
-                
+         
+        list=list.map ( "Pikachu", "Charmander", "Magikarp", "te elijo a ti!")
+           
+
         expect(result).to.have.same.members([
             "Pikachu, te elijo a ti!",
             "Charmander, te elijo a ti!",
